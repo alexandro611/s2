234b77dfc1633d830c2f2731d3ed20ccbf5edc02
sasha <a1leksander@yandex.ru>
2025-04-18 10:43:35 +0300

    fix allure

--- src/test/java/steps/WeatherSteps.java
+++ src/test/java/steps/WeatherSteps.java
@@ -7,11 +7,17 @@ import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
+import io.qameta.allure.*;
+import org.junit.jupiter.api.DisplayName;
 
 
 import static io.restassured.RestAssured.given;
 import static org.junit.jupiter.api.Assertions.*;
 
+
+@Epic("Тесты weatherapi")
+@DisplayName("Тесты weatherapi")
+@Severity(SeverityLevel.CRITICAL)
 public class WeatherSteps {
     private WireMockServer wireMockServer;
     private Response response;
@@ -30,7 +36,7 @@ public class WeatherSteps {
     }
 
 
-
+    @DisplayName("Я запрашиваю погоду для города {string} без авторизации")
     @Given("Я запрашиваю погоду для города {string} без авторизации")
     public void requestWeatherForCityNotAuth(String city) {
         this.cityName = city;
@@ -43,6 +49,7 @@ public class WeatherSteps {
                 .get("/api/weather");
     }
 
+    @DisplayName("Я запрашиваю погоду для города {string} в неизвестных единицах измерения")
     @Given("Я запрашиваю погоду для города {string} в неизвестных единицах измерения")
     public void requestWeatherForCityNotValidUnits(String city) {
         this.cityName = city;
@@ -55,6 +62,7 @@ public class WeatherSteps {
                 .get("/api/weather");
     }
 
+    @DisplayName("Я запрашиваю погоду без указания города")
     @Given("Я запрашиваю погоду без указания города")
     public void requestWeatherForCityWithotCity(String city) {
         this.cityName = city;
@@ -66,6 +74,7 @@ public class WeatherSteps {
                 .get("/api/weather");
     }
 
+    @DisplayName("Я запрашиваю погоду для города {string}")
     @Given("Я запрашиваю погоду для города {string}")
     public void requestWeatherForCity(String city) {
         this.cityName = city;
@@ -78,6 +87,7 @@ public class WeatherSteps {
                 .get("/api/weather");
     }
 
+    @DisplayName("Я получаю корректные данные о погоде")
     @Then("Я получаю корректные данные о погоде")
     public void verifyWeatherData() {
         assertEquals(200, response.getStatusCode());
@@ -86,6 +96,7 @@ public class WeatherSteps {
         assertEquals(cityName, response.jsonPath().get("name"));
     }
 
+    @DisplayName("Температура отображается в градусах Цельсия")
     @And("Температура отображается в градусах Цельсия")
     public void verifyTemperatureInCelsius() {
         String temperature = response.jsonPath().get("main.temp").toString();
@@ -93,12 +104,14 @@ public class WeatherSteps {
         assertEquals("10", response.jsonPath().get("main.temp"));
     }
 
+    @DisplayName("Я получаю сообщение об 404 ошибке город не указан")
     @Then("Я получаю сообщение об 404 ошибке город не указан")
     public void verifyErrorMessage() {
         assertEquals(404, response.getStatusCode());
         assertEquals("city not found", response.jsonPath().get("message"));
     }
 
+    @DisplayName("Я получаю сообщение об 404 ошибке город не задан")
     @Then("Я получаю сообщение об 404 ошибке город не задан")
     public void verifyErrorCity() {
         assertEquals(404, response.getStatusCode());
@@ -106,6 +119,7 @@ public class WeatherSteps {
 
     }
 
+    @DisplayName("Я получаю сообщение об 404 ошибке единицы измерения неизвестны")
     @Then("Я получаю сообщение об 404 ошибке единицы измерения неизвестны")
     public void verifyErrorWeatherMetric() {
         assertEquals(404, response.getStatusCode());
@@ -113,11 +127,10 @@ public class WeatherSteps {
 
     }
 
+    @DisplayName("Я получаю сообщение об 401 ошибке город не задан")
     @Then("Я получаю сообщение об 401 ошибке город не задан")
     public void verifyErrorAuth() {
         assertEquals(401, response.getStatusCode());
 
     }
-
-
 }
